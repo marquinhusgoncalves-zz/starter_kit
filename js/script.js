c1a830feab92789a787068dc0a7e70a9e655dae8
Daniel Mota <danielmota@me.com>
2016-02-06 12:07:34 -0200

    update

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
-
+!function(n){function o(o){var e=n(this),i=n(".mobile-nav");e.toggleClass("is-open"),i.toggleClass("is-open")}var e=n(".mobile-nav-toggle");n(document).ready(function(){e.on("click",o)})}(jQuery);
 //# sourceMappingURL=maps/script.js.map
--- source/js/custom/script.js
+++ source/js/custom/script.js
@@ -1,7 +1,16 @@
 (function ($) {
+    var $mobileNavToggleBtn = $('.mobile-nav-toggle');
+
+    function onBtnClick (e) {
+        var $this = $(this),
+            $selectors = $('.mobile-nav');
+
+        $this.toggleClass('is-open');
+        $selectors.toggleClass('is-open');
+    }
 
     $(document).ready(function () {
-
+        $mobileNavToggleBtn.on('click', onBtnClick);
     });
-    
-});
+
+})(jQuery);
